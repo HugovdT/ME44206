34a8f7cf43d30476c469554763a9dd7bacd65846
Hugovdt <h.p.vonderthusen@student.tudelft.nl>
2024-10-08 22:43:22 +0200

    Added code to repo and started debugging

--- LinearProgrammingModel_assignment1.py
+++ LinearProgrammingModel_assignment1.py
@@ -14,8 +14,8 @@ copper = (0, 4, 2, 5, 3) #% copper
 iron = (82, 56, 73, 65, 87) #% iron
 maxpermonth = (90, 30, 50, 70, 20) #maximum amount of ore that can be supplied per month
 cost = (5, 10, 9, 7, 8.5) #cost per kg of ore
-nidist = (10, 8, 0)
-holdingcosts = {"18/10":20, "18/8":10, "18/0":5}
+nidist = (0.10, 0.8, 0)
+holdingcosts = (20, 10, 5)
 
 # Monthly demand where 18/10, 18/8 and 18/0 are the distributions of chromium and nickel in the stainless steel by percentage
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
@@ -29,7 +29,7 @@ data = {
 monthly_demand = pd.DataFrame(data)
 
 I = range(len(suppliername))                # set of suppliers
-J = range(len(nidists))                     # set of steel types
+J = range(len(nidist))                     # set of steel types
 T = range(len(months))                      # set of months
 
 # ---- Parameters ----
@@ -68,12 +68,76 @@ model.update()
 # Constraint 1: alloy supply
 con1 = {}
 for i in I:
-     con1[i] = model.addConstr(x[i] <= u_i[i], 'con1[' + str(i) + ']-')
+    for t in T:
+        con1[i] = model.addConstr(x[i, t] <= u_i[i], 'con1[' + str(i) + ']-')
 
 # Constraint 2: demand satisfaction
 
 con2 = {}
 for j in J:
     for t in T:
-        con2[j,t] = model.addConstr(p[j,t] + (s[j,t-1] for t in ) >= d_jt[j,t] + s[j,t], 'con2[' + str(j) + ',' + str(t) + ']-')
+        if t == 0:
+            con2[j,t] = model.addConstr(p[j,t] >= (d_jt.iloc[t, j] + s[j,t]), 'con2[' + str(j) + ',' + str(t) + ']-')
+        else:
+            con2[j,t] = model.addConstr((p[j,t] + s[j,t-1]) >= (d_jt.iloc[t, j] + s[j,t]), 'con2[' + str(j) + ',' + str(t) + ']-')
+
+# Constraint 3: max monthly production
+con3 = {}
+for t in T:
+    con3[t] = model.addConstr(quicksum(p[j,t] for j in J) <= 100, 'con3[' + str(t) + ']-')
+
+# Constraint 4: nickel distribution
+con4 = {}
+for t in T:
+    for j in J:
+        con4[t] = model.addConstr(ni_i[j] * p[j, t] == quicksum(ni_i[i] * x[i, t] for i in I), 'con4[' + str(t) + ']-')
+
+# Constraint 5: chromium distribution
+con5 = {}
+for t in T:
+    for j in J:
+        con5[t] = model.addConstr(cr_i[j] * p[j, t] == quicksum(cr_i[i] * x[i, t] for i in I), 'con5[' + str(t) + ']-')
+
+# ---- Solve ----
+
+model.setParam( 'OutputFlag', True) # silencing gurobi output or not
+model.setParam ('MIPGap', 0);       # find the optimal solution
+model.write("output.lp")            # print the model in .lp format file
+
+model.optimize ()
+
+
+# --- Print results ---
+print ('\n--------------------------------------------------------------------\n')
+    
+if model.status == GRB.Status.OPTIMAL: # If optimal solution is found
+    print ('Total cost : %10.2f euro' % model.objVal)
+    print ('')
+    print ('All decision variables:\n')
+
+    k = '%8s' % ''
+    for i in I:
+        k = k + '%8s' % suppliername[i]
+    print (s)    
+
+    for i in I:
+        s = '%8s' % suppliername[i]
+        for i in I:
+            s = s + '%8.3f' % x[i,j].x
+        s = s + '%8.3f' % sum (x[i,j].x for i in I)    
+        print (s)    
+
+    s = '%8s' % ''
+    for i in I:
+        s = s + '%8.3f' % sum (x[i,j].x for j in J)    
+    print (s)    
+
+else:
+    print ('\nNo feasible solution found')
+
+print ('\nREADY\n')
+
+
+
+
 
